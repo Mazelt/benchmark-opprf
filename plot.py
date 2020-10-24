d5fe106cccdae18af37d5483a5b213f25b269f72
Mazelt <marcel.pazelt@gmail.com>
2020-10-24 21:07:54 +0200

    added hash time plotting

--- plot.py
+++ plot.py
@@ -1,8 +1,12 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import os.path
 import glob
 import json
+import argparse
 
+
+batch_name = "ScalingElementsDA_2"
 
 def load_batch(pattern):
     os.path.exists('./batchlogs/experiments')
@@ -31,67 +35,45 @@ def load_batch(pattern):
 
 def plot_hashing(data):
     # simple plot right now.
-    print('Hashing')
-
+    set_sizes = []
+    server_means = []
+    client_means = []
+    server_stds = []
+    client_stds = []
     for b in data:
-        print(b['parameters']['client_neles'])
+        set_sizes.append(b['parameters']['client_neles'])
         s_hashing_t = []
         c_hashing_t = []
         for i in range(len(b)-1):
             s_hashing_t.append(b[i]['s_output']['hashing_t'])
             c_hashing_t.append(b[i]['c_output']['hashing_t'])
-        print(f"Server hashing time: {sum(s_hashing_t)/(len(b)-1)}")
-        print(f"Client hashing time: {sum(c_hashing_t)/(len(b)-1)}")
+        # print(f"Server hashing time: {sum(s_hashing_t)/(len(b)-1)}")
+        # print(f"Client hashing time: {sum(c_hashing_t)/(len(b)-1)}")
+        server_means.append(np.mean(s_hashing_t))
+        server_stds.append(np.std(s_hashing_t))
+        client_means.append(np.mean(c_hashing_t))
+        client_stds.append(np.std(c_hashing_t))
+    x_pos = np.arange(len(set_sizes))
+    fig, ax = plt.subplots()
+    server = ax.bar(x_pos-0.1, server_means, yerr=server_stds, width=0.2, color='b', align='center', alpha=0.5,
+           ecolor='black', capsize=10) 
+    client = ax.bar(x_pos+0.1, client_means, yerr=client_stds, width=0.2, color='g', align='center', alpha=0.5,
+           ecolor='black', capsize=10)
+    ax.set_ylabel(f"Time for hashing in ms")
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(set_sizes)
+    ax.set_title("Hashing times for the server for different set sizes. With std-error.")
+    ax.yaxis.grid(True)
+
+    ax.legend((server[0], client[0]),('server', 'client'))
+
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == '__main__':
-    data = load_batch('ScalingElementsDA_2')
-    plot_hashing(data)
-# def plot_calibration_plots(y_true, probs={}):
-#     # calibration plots (https://scikit-learn.org/stable/auto_examples/calibration/plot_compare_calibration.html)
-#     plt.figure(figsize=(10,10))
-#     ax1 = plt.subplot2grid((3,1), (0,0), rowspan=2)
-#     ax2 = plt.subplot2grid((3,1), (2,0))
-#     ax1.plot([0,1],[0,1], 'k', label='Perfectly calibrated')
-#     for k, v in probs.items():
-#         fraction_of_positives, mean_predicted_value = \
-#             calibration_curve(y_true,v, n_bins=10)
-#         ax1.plot(mean_predicted_value, fraction_of_positives, 's-',
-#                  label=f'{k}')
-#         ax2.hist(v, range=(0,1), bins=10, label=k, histtype='step', lw=2)
-#     ax1.set_ylabel("Fraction of positives")
-#     ax1.set_ylim([-0.05, 1.05])
-#     ax1.legend(loc="lower right")
-#     ax1.set_title('Calibration plots  (reliability curve)')
-
-#     ax2.set_xlabel("Mean predicted value")
-#     ax2.set_ylabel("Count")
-#     ax2.legend(loc="upper center", ncol=2)
-
-#     plt.tight_layout()
-#     plt.show()
-
-
-# def plot_roc_curve(y_true, probs={}):
-#     fpr1, tpr1, _ = metrics.roc_curve(y_true, probs['mpc'])
-#     fpr2, tpr2, _ = metrics.roc_curve(y_true, probs['sklearn'])
-
-#     plt.plot(fpr1, tpr1, marker='.', label='mpc')
-#     plt.plot(fpr2, tpr2,  marker='.', label='sklearn')
-#     plt.xlabel('False Positive Rate')
-#     plt.ylabel('True Positive Rate')
-
-#     plt.legend()
-#     plt.show()
-
-
-# def plot_precision_recall(y_true, probs={}):
-#     mpc_prec, mpc_recall, _ = metrics.precision_recall_curve(
-#         y_true, probs['mpc'])
-#     sklearn_prec, sklearn_recall, _ = metrics.precision_recall_curve(
-#         y_true, probs['sklearn'])
-#     plt.plot(mpc_recall, mpc_prec, marker='.', label='mpc')
-#     plt.plot(sklearn_recall, sklearn_prec, marker='.', label='sklearn')
-#     plt.xlabel('Recall')
-#     plt.ylabel('Precision')
-#     plt.legend()
-#     plt.show()
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--hash', action='store_true')
+    args = ap.parse_args()
+    data = load_batch(batch_name)
+    if args.hash:
+        plot_hashing(data)

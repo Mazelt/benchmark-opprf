ffa77732deb21278d3c0bd525e520202098bc7cd
Mazelt <Pazeltma@hu-berlin.de>
2021-01-02 21:16:19 +0100

    fixing parser to handle scientific notation for long runtimes

--- parsing.py
+++ parsing.py
@@ -6,14 +6,14 @@ re_success_result = re.compile(
 re_app_result = re.compile(r"PSI run returned: (?P<result>\d+)")
 re_time_hashing = re.compile(r"Time for hashing (?P<hashing_t>\d+(\.\d*)?) ms")
 re_time_oprf = re.compile(r"Time for OPRF (?P<oprf_t>\d+(\.\d*)?) ms")
-re_time_poly = re.compile(r"Time for polynomials (?P<poly_t>\d+(\.\d*)?) ms")
+re_time_poly = re.compile(r"Time for polynomials (?P<poly_t>\d+(\.\d*)?(?:[eE][+\-]?\d+)?) ms")
 re_time_poly_trans = re.compile(
-    r"Time for transmission of the polynomials (?P<poly_trans_t>\d+(\.\d*)?) ms")
+    r"Time for transmission of the polynomials (?P<poly_trans_t>\d+(\.\d*)?(?:[eE][+\-]?\d+)?) ms")
 re_time_aby = re.compile(
-    r"ABY timings: online time (?P<aby_online_t>\d+(\.\d*)?) ms, setup time (?P<aby_setup_t>\d+(\.\d*)?) ms, total time (?P<aby_total_t>\d+(\.\d*)?) ms, base OTs time (?P<aby_baseot_t>\d+(\.\d*)?) ms")
-re_time_total = re.compile(r"Total runtime: (?P<total_t>\d+(\.\d*)?)ms")
+    r"ABY timings: online time (?P<aby_online_t>\d+(\.\d*)?(?:[eE][+\-]?\d+)?) ms, setup time (?P<aby_setup_t>\d+(\.\d*)?(?:[eE][+\-]?\d+)?) ms, total time (?P<aby_total_t>\d+(\.\d*)?(?:[eE][+\-]?\d+)?) ms, base OTs time (?P<aby_baseot_t>\d+(\.\d*)?(?:[eE][+\-]?\d+)?) ms")
+re_time_total = re.compile(r"Total runtime: (?P<total_t>\d+(\.\d*)?(?:[eE][+\-]?\d+)?)ms")
 re_time_nobase = re.compile(
-    r"Total runtime w/o base OTs: (?P<nobase_t>\d+(\.\d*)?)ms")
+    r"Total runtime w/o base OTs: (?P<nobase_t>\d+(\.\d*)?(?:[eE][+\-]?\d+)?)ms")
 re_comm_poly = re.compile(
     r"Data for polynomials recv/sent (?P<recv>\d+) / (?P<sent>\d+) b")
 re_comm_oprf = re.compile(
@@ -186,15 +186,14 @@ class Parser(object):
 
 if __name__ == '__main__':
     to_parse = "PSI circuit successfully executed. Result: 0\n" + \
-    "AES performance: 4589.05 MiB/sec\n" + \
     "RTT: 2.43131 ms\n" + \
     "Throughput: 51.2355 MiB/s\n" + \
     "Time for hashing 6.59715 ms\n" + \
     "Time for OPRF 452.343 ms\n" + \
     "Time for polynomials 365.963 ms\n" + \
-    "Time for transmission of the polynomials 0.072548 ms\n" + \
+    "Time for transmission of the polynomials 2310.0 ms\n" + \
     "ABY timings: online time 2.669 ms, setup time 88.571 ms, total time 91.24 ms, base OTs time 222.2 ms\n" + \
-    "Total runtime: 1250.27ms\n" + \
+    "Total runtime: 1234ms\n" + \
     "Total runtime w/o base OTs: 583.952ms\n" + \
     "Data for polynomials recv/sent 0 / 124800 b\n" + \
     "Data for oprf recv/sent 332961 / 16944 b\n" + \
@@ -204,10 +203,33 @@ if __name__ == '__main__':
     "Total sent: 5254659 bytes\n" + \
     "Total recv w/o base OTs: 5395847 b\n" + \
     "Total sent w/o base OTs: 5187803 b\n"
-    print(to_parse)
+    to_parse_2 = "PSI circuit successfully executed. Result: 0\n" + \
+    "RTT: 2.43131 ms\n" + \
+    "Throughput: 51.2355 MiB/s\n" + \
+    "Time for hashing 6.59715 ms\n" + \
+    "Time for OPRF 452.343 ms\n" + \
+    "Time for polynomials 365.963 ms\n" + \
+    "Time for transmission of the polynomials 1.25013e+06 ms\n" + \
+    "ABY timings: online time 2 ms, setup time 1.25013e+02 ms, total time 91.24 ms, base OTs time 222.2 ms\n" + \
+    "Total runtime: 1234ms\n" + \
+    "Total runtime w/o base OTs: 583.952ms\n" + \
+    "Data for polynomials recv/sent 0 / 124800 b\n" + \
+    "Data for oprf recv/sent 332961 / 16944 b\n" + \
+    "ABY recv: online 117750 bytes, setup 4995165 bytes, total 5112915 bytes, base OTs 43434 bytes\n" + \
+    "ABY sent: online 117750 bytes, setup 4995165 bytes, total 5112915 bytes, base OTs 43434 bytes\n" + \
+    "Total recv: 5445876 bytes\n" + \
+    "Total sent: 5254659 bytes\n" + \
+    "Total recv w/o base OTs: 5395847 b\n" + \
+    "Total sent w/o base OTs: 5187803 b\n"
+    # print(to_parse)
     import logging
     logger = logging.getLogger('__name__')
 
     parser = Parser(logger)
     data = parser.parse_output(to_parse)
+    print(data == {'result': 0, 'rtt': 2.43131, 'throughput': 51.2355, 'hashing_t': 6.59715, 'oprf_t': 452.343, 'poly_t': 365.963, 'poly_trans_t': 2310.0, 'aby_online_t': 2.669, 'aby_setup_t': 88.571, 'aby_total_t': 91.24, 'aby_baseot_t': 222.2, 'total_t': 1234.0, 'nobase_t': 583.952, 'poly_d_rs': [0, 124800], 'oprf_d_rs': [332961, 16944], 'aby_online_d_rs': [117750, 117750], 'aby_setup_d_rs': [4995165, 4995165], 'aby_total_d_rs': [5112915, 5112915], 'aby_baseot_d_rs': [43434, 43434], 'total_d_rs': [5445876, 5254659], 'nobase_d_rs': [5395847, 5187803]})
     print(data)
+    # print(to_parse_2)
+    data2 = parser.parse_output(to_parse_2)
+    print(data2== {'result': 0, 'rtt': 2.43131, 'throughput': 51.2355, 'hashing_t': 6.59715, 'oprf_t': 452.343, 'poly_t': 365.963, 'poly_trans_t': 1250130.0, 'aby_online_t': 2.0, 'aby_setup_t': 125.013, 'aby_total_t': 91.24, 'aby_baseot_t': 222.2, 'total_t': 1234.0, 'nobase_t': 583.952, 'poly_d_rs': [0, 124800], 'oprf_d_rs': [332961, 16944], 'aby_online_d_rs': [117750, 117750], 'aby_setup_d_rs': [4995165, 4995165], 'aby_total_d_rs': [5112915, 5112915], 'aby_baseot_d_rs': [43434, 43434], 'total_d_rs': [5445876, 5254659], 'nobase_d_rs': [5395847, 5187803]})
+    print(data2)

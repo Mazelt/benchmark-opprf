7adc50cb145c8b0adfea30b38f62da40ca89c6ae
Mazelt <Pazeltma@hu-berlin.de>
2021-03-04 12:50:38 +0100

    smol parse_iperf changes

--- network_measure/parse_iperf.py
+++ network_measure/parse_iperf.py
@@ -9,18 +9,18 @@ def get_tps(file_name):
     tps = []
 
     # remove ramp-up phase and get only 25 runs
-    start = 10
-    max_v = 35
+    start = 0
+    max_v = 25
     for i, inter in enumerate(data['intervals']):
-        if i < start:
-            continue
-        elif i == max_v:
-            break
+        # if i < start:
+        #     continue
+        # elif i == max_v:
+        #     break
         tps.append(inter['sum']['bits_per_second']/1e6)
 
     print(f"{file_name} Measures: {len(tps)}, Avg: {np.mean(tps):.2f} Mbit/s, SD: {np.std(tps):0.4f}")
 
-fnames = ["LAN_2500_c.json","LAN_2500_s.json","WAN_500_c.json","WAN_500_s.json","LTE_50_c.json","LTE_50_s.json"]
+fnames = ["LAN_2500_c.json","LAN_2500_s.json","WAN_500_c.json","WAN_500_s.json","LTE_200_c.json","LTE_50_s.json"]
 
 for f in fnames:
     get_tps(f)
